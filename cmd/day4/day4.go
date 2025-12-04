f0428ece4d6aced6617995975d185692d2013fcc
Welf Walter <welf.walter@gmail.com>
2025-12-04 20:33:47 +0100

    day4: input parsing

---
+++ cmd/day4/day4.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Grid [][]bool
+
+func parseInput(input string) Grid {
+	grid := Grid{}
+	line_iter := strings.SplitSeq(input, "\n")
+	for line := range line_iter {
+		gridline := []bool{}
+		for _, c := range line {
+			switch c {
+			case '.':
+				gridline = append(gridline, false)
+			case '@':
+				gridline = append(gridline, true)
+			default:
+				panic(fmt.Sprintf("Unexpected character %v in %v", c, line))
+			}
+		}
+		grid = append(grid, gridline)
+	}
+	return grid
+}
+
+func isPaper(grid Grid, x int, y int) bool {
+	if y < 0 || y >= len(grid) {
+		return false
+	}
+	gridline := grid[y]
+	if x < 0 || x >= len(gridline) {
+		return false
+	}
+	return gridline[x]
+}
---
+++ cmd/day4/day4_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert" // https://pkg.go.dev/github.com/stretchr/testify/assert
+)
+
+const example string = `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+
+func TestParse(t *testing.T) {
+	assert := assert.New(t)
+
+	grid := parseInput(example)
+	assert.False(isPaper(grid, -1, 0))
+	assert.False(isPaper(grid, 0, -1))
+	assert.True(isPaper(grid, 0, 1))
+}
