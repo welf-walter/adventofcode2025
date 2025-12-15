ce9cf237ab3d1622216149a75d389b48464d9926
Welf Walter <welf.walter@gmail.com>
2025-12-15 22:38:12 +0100

    day 11: unit tests succeed

--- cmd/day11/day11_test.go
+++ cmd/day11/day11_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"adventofcode/year2025/cmd/optimize"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,4 +26,29 @@ func TestParsing(t *testing.T) {
 	assert.Equal([]string{"ddd", "eee", "fff"}, graph.GetTargets("ccc"))
 	assert.Equal([]string{"out"}, graph.GetTargets("iii"))
 
+}
+
+func pathToStringList(path optimize.Path) []string {
+	names := make([]string, len(path))
+	for index, node := range path {
+		names[index] = node.(optimize.SimpleNode).String()
+	}
+	return names
+}
+
+func Test1(t *testing.T) {
+	assert := assert.New(t)
+	graph := parseInput(example)
+
+	pathes := []optimize.Path{}
+	optimize.ForAllPathes(graph.FindNode(startNodeName), func(path optimize.Path) {
+		pathes = append(pathes, path)
+	})
+
+	assert.Equal(5, len(pathes))
+	assert.Equal([]string{"you", "bbb", "ddd", "ggg", "out"}, pathToStringList(pathes[0]))
+	assert.Equal([]string{"you", "bbb", "eee", "out"}, pathToStringList(pathes[1]))
+	assert.Equal([]string{"you", "ccc", "ddd", "ggg", "out"}, pathToStringList(pathes[2]))
+	assert.Equal([]string{"you", "ccc", "eee", "out"}, pathToStringList(pathes[3]))
+	assert.Equal([]string{"you", "ccc", "fff", "out"}, pathToStringList(pathes[4]))
 }
