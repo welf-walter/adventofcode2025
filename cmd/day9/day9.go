bad58194549d750ba8f4df2f1a0e543382d4e071
Welf Walter <welf.walter@gmail.com>
2025-12-19 23:35:38 +0100

    day 9: floor handling

--- cmd/day9/day9.go
+++ cmd/day9/day9.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventofcode/year2025/cmd/util"
 	"fmt"
+	"log"
 	"strings"
 )
 
@@ -34,6 +35,78 @@ func calcArea(a, b redTile) int {
 func largestRectangle(tiles []redTile) (area int) {
 	util.ForAllPairs(tiles, func(a, b redTile) { area = max(area, calcArea(a, b)) })
 	return
+}
+
+// per line, from where to where are red or green tiles?
+type tileFloor struct {
+	lines []struct{ from, to int }
+}
+
+func newTileFloor(height int) tileFloor {
+	return tileFloor{lines: make([]struct {
+		from int
+		to   int
+	}, height)}
+}
+
+func (floor *tileFloor) put(x, y int) {
+	line := floor.lines[y]
+	//if line.from != line.to {
+	//	if line.from != x && line.to != x {
+	//		panic(fmt.Sprintf("Unexpected: More than two points in one row: %v,%v", x, y))
+	//	}
+	//}
+	if line.from == 0 {
+		line.from = x
+		line.to = x
+	} else {
+		line.from = min(line.from, x)
+		line.to = max(line.to, x)
+	}
+	floor.lines[y] = line
+}
+
+func (floor *tileFloor) putLine(a redTile, b redTile) {
+	log.Printf("Line from (%v,%v) to (%v,%v)", a.x, a.y, b.x, b.y)
+	if a.y == b.y {
+		floor.put(a.x, a.y)
+		floor.put(b.x, b.y)
+		return
+	}
+	if a.x != b.x {
+		panic("x != x and y != y")
+	}
+	miny := min(a.y, b.y)
+	maxy := max(a.y, b.y)
+	for y := miny; y <= maxy; y++ {
+		floor.put(a.x, y)
+	}
+}
+
+func (floor *tileFloor) coversRectangle(a redTile, b redTile) bool {
+	miny := min(a.y, b.y)
+	maxy := max(a.y, b.y)
+	minx := min(a.x, b.x)
+	maxx := max(a.x, b.x)
+	for y := miny; y <= maxy; y++ {
+		if floor.lines[y].from > minx {
+			log.Printf("In line %v, floor is %v..%v but not %v", y, floor.lines[y].from, floor.lines[y].to, minx)
+			return false
+		}
+		if floor.lines[y].to < maxx {
+			log.Printf("In line %v, floor is %v..%v but not %v", y, floor.lines[y].from, floor.lines[y].to, maxx)
+			return false
+		}
+	}
+	return true
+}
+
+func connectTiles(floor *tileFloor, tiles []redTile) {
+	for i := range tiles {
+		a := tiles[i]
+		b := tiles[(i+1)%len(tiles)]
+		floor.putLine(a, b)
+	}
 }
 
 func main() {
--- cmd/day9/day9_test.go
+++ cmd/day9/day9_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,10 +16,24 @@ const example = `7,1
 2,3
 7,3`
 
-func TestParsing(t *testing.T) {
+func Test1(t *testing.T) {
 	assert := assert.New(t)
 	tiles := parseInput(example)
 	assert.Equal(8, len(tiles))
 	assert.Equal(redTile{11, 7}, tiles[2])
 	assert.Equal(50, largestRectangle(tiles))
+}
+
+func Test2(t *testing.T) {
+	assert := assert.New(t)
+	tiles := parseInput(example)
+	floor := newTileFloor(12)
+	connectTiles(&floor, tiles)
+	log.Print(floor)
+
+	assert.True(floor.coversRectangle(redTile{7, 3}, redTile{11, 1}))
+	assert.False(floor.coversRectangle(redTile{6, 3}, redTile{11, 1}))
+	assert.False(floor.coversRectangle(redTile{7, 3}, redTile{11, 0}))
+	assert.True(floor.coversRectangle(redTile{9, 7}, redTile{9, 5}))
+	assert.True(floor.coversRectangle(redTile{9, 5}, redTile{2, 3}))
 }
