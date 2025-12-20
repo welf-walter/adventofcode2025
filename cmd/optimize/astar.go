2600739e6851a285aedb738b87b0cf32f170cffc
Welf Walter <welf.walter@gmail.com>
2025-12-20 23:10:33 +0100

    A*: implementation

--- cmd/optimize/astar.go
+++ cmd/optimize/astar.go
@@ -1,5 +1,11 @@
 package optimize
 
+import (
+	"log"
+	"math"
+	"slices"
+)
+
 // https://de.wikipedia.org/wiki/A*-Algorithmus
 
 type AstarNode interface {
@@ -12,4 +18,93 @@ type AstarNode interface {
 type AstarAction interface {
 	targetNode(startNode AstarNode) AstarNode
 	actualCost(startNode AstarNode) int
+}
+
+type AstarPath []AstarNode
+
+type nodeWithCost struct {
+	node AstarAction
+	cost int
+}
+type nodeCost map[AstarNode]int
+
+func dummy() {
+
+}
+func findBestNode(costMap nodeCost) (bestNode AstarNode, cost int) {
+	mini := math.MaxInt
+
+	for n := range costMap {
+		mini = min(mini, costMap[n])
+	}
+
+	for n := range costMap {
+		if costMap[n] == mini {
+			return n, mini
+		}
+	}
+	panic("no best node found")
+}
+
+func findMinimalPath(startNode AstarNode) (bestPath AstarPath, cost int) {
+
+	//todoList := []nodeWithCost{startNode,0}
+	todoList := map[AstarNode]int{}
+	todoList[startNode] = 0
+
+	predecessor := map[AstarNode]AstarNode{}
+
+	doneList := map[AstarNode]bool{}
+
+	for {
+
+		if len(todoList) == 0 {
+			panic("No solution found!")
+		}
+
+		currentNode, currentCost := findBestNode(todoList)
+		log.Printf("Process node %v (cost = %v)", currentNode, currentCost)
+		delete(todoList, currentNode)
+
+		if currentNode.estimatedCost() == 0 {
+			// build up path
+			bestPath = make(AstarPath, 0)
+			for n := currentNode; n != startNode; n = predecessor[n] {
+				bestPath = append(bestPath, n)
+			}
+			bestPath = append(bestPath, startNode)
+			slices.Reverse(bestPath)
+			return bestPath, currentCost
+
+		}
+		doneList[currentNode] = true
+
+		actions := currentNode.possibleActions()
+		for _, action := range actions {
+			log.Printf("  expand action %v", action)
+			target := action.targetNode(currentNode)
+			if doneList[target] {
+				log.Printf("  been there. done that.")
+				continue
+			}
+
+			actualCost := action.actualCost(currentNode)
+			newCost := currentCost + actualCost
+
+			costUpToNow, alternativeWay := todoList[target]
+			if alternativeWay {
+				if costUpToNow > newCost {
+					log.Printf("  found better way to %v. %v + %v < %v", target, currentCost, actualCost, costUpToNow)
+					todoList[target] = newCost
+					predecessor[target] = currentNode
+				} else {
+					log.Printf("  no better way to %v. %v + %v >= %v", target, currentCost, actualCost, costUpToNow)
+				}
+			} else {
+				log.Printf("  found way to %v.", target)
+				todoList[target] = newCost
+				predecessor[target] = currentNode
+			}
+		}
+	}
 }
--- cmd/optimize/astar_test.go
+++ cmd/optimize/astar_test.go
@@ -1,6 +1,7 @@
 package optimize
 
 import (
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,7 +27,7 @@ type action struct {
 func start() position  { return position{0, 2} }
 func target() position { return position{4, 2} }
 
-func (p *position) estimatedCost() int {
+func (p position) estimatedCost() int {
 	return 0 +
 		max(p.x-target().x, target().x-p.x) +
 		max(p.y-target().y, target().y-p.y)
@@ -42,7 +43,7 @@ func positionOk(p position) bool {
 	return maze()[p.y][p.x] != '#'
 }
 
-func (p *position) possibleActions() (actions []action) {
+func (p position) possibleActions() (actions /*[]action*/ []AstarAction) {
 	if positionOk(position{p.x - 1, p.y}) {
 		actions = append(actions, action{-1, 0})
 	}
@@ -58,20 +59,44 @@ func (p *position) possibleActions() (actions []action) {
 	return
 }
 
-func (a *action) targetNode(startNode position) position {
-	return position{startNode.x + a.dx, startNode.y + a.dy}
+func (a action) targetNode(startNode /*position*/ AstarNode) AstarNode {
+	pos := startNode.(position)
+	return position{pos.x + a.dx, pos.y + a.dy}
 }
 
-func (a *action) actualCost(startNode position) int {
+func (a action) actualCost(startNode /*position*/ AstarNode) int {
 	return 1
+}
+
+func concreteActionList(in []AstarAction) (out []action) {
+	out = make([]action, len(in))
+	for i := range in {
+		out[i] = in[i].(action)
+	}
+	return
 }
 
 func TestAstar(t *testing.T) {
 	assert := assert.New(t)
 
 	s := start()
-	assert.Equal([]action{{0, -1}, {1, 0}, {0, +1}}, s.possibleActions())
+	assert.Equal([]action{{0, -1}, {1, 0}, {0, +1}}, concreteActionList(s.possibleActions()))
 
 	assert.Equal(4, s.estimatedCost())
+
+	path, mini := findMinimalPath(s)
+	assert.Equal(8, mini)
+	/*	assert.Equal(AstarPath{
+		position{0, 2},
+		position{0, 1},
+		position{0, 0},
+		position{1, 0},
+		position{2, 0},
+		position{3, 0},
+		position{4, 0},
+		position{4, 1},
+		position{4, 2},
+	}, path)*/
+	log.Print(path)
 
 }
